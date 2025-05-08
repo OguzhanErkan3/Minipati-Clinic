708d47e70b53e969fdd3b561de94e57d22115772
Oğuzhan ERKAN <101500204+OguzhanErkan3@users.noreply.github.com>
2025-05-08 19:10:15 +0300

    feat: add next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** next-sitemap.config.js */
+module.exports = {
+  siteUrl: 'https://www.minipativeteriner.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
